d5083c008b9a96e3a260b2e3c48e4761f29a1751
Kirill Tuchkin <volkirtu@gmail.com>
2023-04-29 00:02:44 +0300

    Basic bot created

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
-from constants import TOKEN
+from config import TG_TOKEN
+from parser import get_weather
 
 import telebot
 
-bot = telebot.TeleBot(TOKEN, parse_mode=None)
+bot = telebot.TeleBot(TG_TOKEN, parse_mode='html')
 
 
 @bot.message_handler(commands=['start'])
 def welcome_message(message):
-    bot.reply_to(message, "привет")
+    weather_data = get_weather()
+
+    temperature = weather_data["fact"]["temp"]
+    feels_like = weather_data["fact"]["feels_like"]
+
+    bot.send_message(message.chat.id, f"Привет,\nПогода в Москве:\n"
+                                      f"<b>Температура:</b> {temperature}°C\n"
+                                      f"<b>Ощущается как: </b>{feels_like}°C")
 
 
 if __name__ == "__main__":
-    bot.infinity_polling()
+    bot.polling(none_stop=True)

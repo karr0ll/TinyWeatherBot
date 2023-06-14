2cb424b3571485e4ff0818eda06eca7066378678
Kirill Tuchkin <volkirtu@gmail.com>
2023-06-14 11:04:13 +0300

    добавлена реакция на ввод названия города

--- api_handlers/dadata_api_handler.py
+++ api_handlers/dadata_api_handler.py
@@ -7,7 +7,12 @@ from dadata import Dadata
 
 class DadataAPIHandler:
 
-    def __init__(self, lat: float, lon: float) -> None:
+    def __init__(
+            self,
+            lat: float = None,
+            lon: float = None,
+            city: str = None
+    ) -> None:
         """
         Класс инициализируется координатами из геолокации пользователя и
         :param lat: широта
@@ -17,6 +22,7 @@ class DadataAPIHandler:
         """
         self.lat = lat
         self.lon = lon
+        self.city = city
 
     def get_city_data_by_coords(self) -> list[dict]:
         """
@@ -26,14 +32,24 @@ class DadataAPIHandler:
         :rtype: list[dict]
         """
         load_dotenv()
-        dadata_token: str = os.environ.get("DADATA_TOKEN")
-        token = dadata_token
-        dadata = Dadata(token)
-        city_data = dadata.geolocate(
+        DADATA_TOKEN: str = os.environ.get("DADATA_TOKEN")
+        dadata = Dadata(DADATA_TOKEN)
+        city_from_coords_data = dadata.geolocate(
             name="address",
             lat=self.lat,
             lon=self.lon,
             radius_meters=500,
             count=1
         )
-        return city_data
+        return city_from_coords_data
+
+    def get_coords_by_city(self) -> dict:
+        load_dotenv()
+        DADATA_TOKEN: str = os.environ.get("DADATA_TOKEN")
+        DADATA_SECRET: str = os.environ.get("DADATA_SECRET")
+        dadata = Dadata(DADATA_TOKEN, DADATA_SECRET)
+        coords_from_city_data = dadata.clean(
+            "address", self.city
+        )
+        return coords_from_city_data
+
--- main.py
+++ main.py
@@ -12,12 +12,12 @@ load_dotenv()
 tg_token: str = os.environ.get("TG_TOKEN")
 
 bot = Bot(token=tg_token, parse_mode="HTML")
-dp = Dispatcher()
+dispatcher = Dispatcher()
 user_shared_location = []
 
 
-@dp.message(Command("start"))
-async def get_geolocation_data(message: types.Message):
+@dispatcher.message(Command("start"))
+async def ask_for_location(message: types.Message):
     buttons = [[types.KeyboardButton(
         text="Поделиться местоположением",
         request_location=True
@@ -30,24 +30,44 @@ async def get_geolocation_data(message: types.Message):
     await message.answer("Отправьте свое местоположение или введите название города",
                          reply_markup=keyboard)
 
-# TODO: добавить получение города с ввода пользователя
 
+@dispatcher.message(F.text)
+async def send_current_weather_by_city(message: types.Message):
+    city = message.text.capitalize()
+    city_data_api_handler = DadataAPIHandler(city=city)
+    city_coords = city_data_api_handler.get_coords_by_city()
+    if city_coords["result"] is None:
+        await message.answer("Такой город не найден")
+    else:
+        lat = city_coords["geo_lat"]
+        lon = city_coords["geo_lon"]
+        city_type = city_coords["region_type"]
+        city_name = city_coords["region"]
 
-@dp.message(F.location)
-async def send_forecast(message: types.Message):
+        weather_api_handler = WeatherAPIHandler(lat, lon)
+        weather_data = weather_api_handler.get_weather()
+        temperature = weather_data["fact"]["temp"]
+        feels_like = weather_data["fact"]["feels_like"]
+
+        reply = f"Погода в {city_type}.{city_name} сейчас:\n" \
+                f"<b>Температура: </b>{temperature}°C\n" \
+                f"<b>Ощущается как: </b>{feels_like}°C"
+        await message.answer(reply)
+
+@dispatcher.message(F.location)
+async def send_current_weather_by_location(message: types.Message):
     lat: float = message.location.latitude
     lon: float = message.location.longitude
 
     weather_api_handler = WeatherAPIHandler(lat, lon)
     weather_data = weather_api_handler.get_weather()
 
-    city_data_api_handler = DadataAPIHandler(lat, lon)
+    city_data_api_handler = DadataAPIHandler(lat=lat, lon=lon)
     city_data = city_data_api_handler.get_city_data_by_coords()
 
     temperature = weather_data["fact"]["temp"]
     feels_like = weather_data["fact"]["feels_like"]
 
-    # for item in city_data:
     city_type = city_data[0]["data"]["region_type"]
     city_name = city_data[0]["data"]["region"]
 
@@ -59,7 +79,7 @@ async def send_forecast(message: types.Message):
 
 
 async def main():
-    await dp.start_polling(bot)
+    await dispatcher.start_polling(bot)
 
 
 if __name__ == "__main__":
